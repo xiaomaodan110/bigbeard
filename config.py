4bea3bd900c49f1890d7a1b749f0ac52daff153e
Andy <13866558655@163.com>
2022-06-03 22:43:04 +0800

    更新关于我们的说明

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import pyaudio
 
 ICO_LOGO = os.path.dirname(__file__) + '/ico/record256.ico'
 TITLE = '大胡子录屏 测试版 v0.1'
-ABOUT_US_TEXT = '欢迎使用大胡子录屏软件。\n当前版本:测试版 v0.1。\n\n该软件基于GPL协议开源,不可用于任何商业用途。\n项目地址:  https://github.com/xiaomaodan110/bigbeard  \n\n如您对软件有任何宝贵的建议和其他问题,可加QQ群 582727724'
+ABOUT_US_TEXT = '欢迎使用大胡子录屏软件。\n当前版本:测试版 v0.1。\n\n该软件基于GPL协议开源,不可用于任何商业用途。\n项目地址:  https://github.com/xiaomaodan110/bigbeard'
 USER_CONFIG_PATH = os.path.dirname(__file__) + '/大胡子录屏配置文件.cfg'
 
 # 录音配置
